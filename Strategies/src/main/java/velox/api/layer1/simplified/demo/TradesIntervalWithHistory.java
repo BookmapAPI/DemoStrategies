ea8900c0c2e6111a004b604a4d0970ff1e6673dc
Svyatoslav Toder <svyatoslav@veloxpro.com>
2018-10-04 12:10:29 +0300

    feat(BMDEV-2407):  demo TradesIntervalWithHistory
    BMDEV-2407 Simplified API wrapper: Iteration 2 - per instrument wrapper

---
+++ Strategies/src/main/java/velox/api/layer1/simplified/demo/TradesIntervalWithHistory.java
@@ -0,0 +1,15 @@
+package velox.api.layer1.simplified.demo;
+
+import velox.api.layer1.annotations.Layer1ApiVersion;
+import velox.api.layer1.annotations.Layer1ApiVersionValue;
+import velox.api.layer1.annotations.Layer1SimpleAttachable;
+import velox.api.layer1.annotations.Layer1StrategyName;
+import velox.api.layer1.simplified.HistoricalDataListener;
+
+@Layer1SimpleAttachable
+@Layer1StrategyName("Trade interval: with history")
+@Layer1ApiVersion(Layer1ApiVersionValue.VERSION1)
+public class TradesIntervalWithHistory extends TradesIntervalNoHistory 
+    implements HistoricalDataListener {
+
+}

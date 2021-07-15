594c12641d896fff9ec601683a48e3e71f5a545c
Igor Kozlinskyi <ihor.kozlinskyi.dev@gmail.com>
2021-07-15 14:15:45 +0300

    feat(BMDEV-4464): SimplePriceAlertDemo - don't send alerts if declaration is removed
    - add comments for SimplePriceAlertDemo
    
    BMDEV-4464 Notification system: phase 3
    BMDEV-4100 Improve L1 notification system

--- Strategies/src/main/java/velox/api/layer1/simpledemo/alerts/tradeprice/SimplePriceAlertDemo.java
+++ Strategies/src/main/java/velox/api/layer1/simpledemo/alerts/tradeprice/SimplePriceAlertDemo.java
@@ -13,6 +13,7 @@ import velox.api.layer1.common.ListenableHelper;
 import velox.api.layer1.data.TradeInfo;
 import velox.api.layer1.messages.Layer1ApiAlertSettingsMessage;
 import velox.api.layer1.messages.Layer1ApiSoundAlertDeclarationMessage;
+import velox.api.layer1.messages.Layer1ApiSoundAlertDeclarationMessage.Builder;
 import velox.api.layer1.messages.Layer1ApiSoundAlertMessage;
 
 @Layer1Attachable
@@ -32,6 +33,10 @@ public class SimplePriceAlertDemo implements
     
         ListenableHelper.addListeners(provider, this);
     
+        /*
+         * The declaration message helps Bookmap to create controls for this alert,
+         * for more info check out Layer1ApiSoundAlertDeclarationMessage javadoc
+         */
         declarationMessage = Layer1ApiSoundAlertDeclarationMessage.builder()
             .setTriggerDescription("Trade price > 10")
             .setSource(SimplePriceAlertDemo.class)
@@ -40,6 +45,10 @@ public class SimplePriceAlertDemo implements
             .build();
         provider.sendUserMessage(declarationMessage);
     
+        /*
+         * The settings message determines whether an alert has sound notification,
+         * alert popup. For more info check out Layer1ApiAlertSettingsMessage javadoc
+         */
         settingsMessage = Layer1ApiAlertSettingsMessage
             .builder()
             .setDeclarationId(declarationMessage.id)
@@ -54,20 +63,41 @@ public class SimplePriceAlertDemo implements
         if (price > 10) {
             System.out.printf("Trade of price > 10 occurred, actual price={%.2f}, size={%d}%n", price, size);
             
-            Layer1ApiSoundAlertMessage soundAlertMessage = Layer1ApiSoundAlertMessage.builder()
-                .setAlias(alias)
-                .setTextInfo(String.format("Trade actual price={%.2f}, size={%d}%n", price, size))
-                .setAdditionalInfo("Trade of price > 10")
-                .setShowPopup(settingsMessage.popup)
-                .setAlertDeclarationId(declarationMessage.id)
-                .setSource(SimplePriceAlertDemo.class)
-                .build();
-            provider.sendUserMessage(soundAlertMessage);
+            /*
+             * The actual alert is sent here. Note that it is connected to the
+             * declaration message via alertDeclarationId, and the state of the
+             * popup (on/off) is taken from the settingsMessage. And, the alert
+             * message is sent AFTER the declaration and settings messages were sent
+             * (from the constructor).
+             *
+             * If the declarationMessage is null - it means that the alert was
+             * removed by a user - take a look at #onUserMessage(Object), where
+             * this field is nullified if the arrived declaration message has flag
+             * isAdd = false
+             */
+            if (declarationMessage != null) {
+                Layer1ApiSoundAlertMessage soundAlertMessage = Layer1ApiSoundAlertMessage.builder()
+                    .setAlias(alias)
+                    .setTextInfo(String.format("Trade actual price={%.2f}, size={%d}%n", price, size))
+                    .setAdditionalInfo("Trade of price > 10")
+                    .setShowPopup(settingsMessage.popup)
+                    .setAlertDeclarationId(declarationMessage.id)
+                    .setSource(SimplePriceAlertDemo.class)
+                    .build();
+                provider.sendUserMessage(soundAlertMessage);
+            }
+
         }
     }
     
     @Override
     public void onUserMessage(Object data) {
+        /*
+         * We need to listen for Layer1ApiSoundAlertDeclarationMessage - as the alert
+         * might be removed by the user
+         * And for the Layer1ApiAlertSettingsMessage - as settings can be changed
+         * by the user
+         */
         if (data instanceof Layer1ApiSoundAlertDeclarationMessage) {
             Layer1ApiSoundAlertDeclarationMessage obtainedDeclarationMessage = (Layer1ApiSoundAlertDeclarationMessage) data;
             if (obtainedDeclarationMessage.source == SimplePriceAlertDemo.class && !obtainedDeclarationMessage.isAdd) {
@@ -83,5 +113,17 @@ public class SimplePriceAlertDemo implements
     
     @Override
     public void finish() {
+        /*
+         * Although Bookmap will try its best to remove any stale alerts
+         * when your addon is unloaded, it is a good practice to take care of the
+         * resources you used.
+         * Also, it is a suitable place to show how you can remove the alert declaration
+         */
+        if (declarationMessage != null) {
+            Layer1ApiSoundAlertDeclarationMessage removeDeclarationMessage = new Builder(declarationMessage)
+                .setIsAdd(false)
+                .build();
+            provider.sendUserMessage(removeDeclarationMessage);
+        }
     }
 }

8a7688e3d57eaa4e92945058211d925e565a88cc
Arthur Krukowski <arthurk@bookmap.com>
2022-07-04 11:13:04 +0300

    feat(BMDEV-5126): add action demo strategies
    BMDEV-5126 Window tagging for shortcuts

---
+++ Strategies/src/main/java/velox/api/layer1/simpledemo/actions/ActionStrategy2.java
@@ -0,0 +1,149 @@
+package velox.api.layer1.simpledemo.actions;
+
+import velox.api.layer1.Layer1ApiFinishable;
+import velox.api.layer1.Layer1ApiProvider;
+import velox.api.layer1.actions.Layer1ActionContainer;
+import velox.api.layer1.actions.Layer1ExternalAction;
+import velox.api.layer1.actions.annotations.Layer1ActionMapper;
+import velox.api.layer1.actions.annotations.Layer1ActionMetadata;
+import velox.api.layer1.annotations.Layer1ApiVersion;
+import velox.api.layer1.annotations.Layer1ApiVersionValue;
+import velox.api.layer1.annotations.Layer1Injectable;
+import velox.api.layer1.annotations.Layer1StrategyName;
+import velox.api.layer1.data.InstrumentInfo;
+import velox.api.layer1.layers.Layer1ApiInjectorRelay;
+import velox.api.layer1.messages.Layer1ApiSoundAlertMessage;
+
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
+import java.awt.FlowLayout;
+import java.awt.event.ItemEvent;
+import java.awt.event.KeyEvent;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Simple strategy that declares actions and executes them in its external window.
+ * It declares custom action that sends message alerts when focus is on the external window.
+ *
+ * To be able to perform actions in external window, such windows must have
+ * {@link Layer1ExternalAction#EXTERNAL_WINDOW_ALIAS_PROPERTY} client property.
+ */
+@Layer1Injectable
+@Layer1StrategyName(ActionStrategy2.STRATEGY_NAME)
+@Layer1ApiVersion(Layer1ApiVersionValue.VERSION2)
+public class ActionStrategy2 extends Layer1ApiInjectorRelay implements Layer1ApiFinishable {
+    static final String STRATEGY_NAME = "Action Strategy 2";
+    private static final String NONE_ITEM = "---";
+    
+    private static JFrame frame;
+    private static JComboBox<String> combo;
+    private Layer1ApiProvider provider;
+    private JLabel aliasLabel;
+
+    public ActionStrategy2(Layer1ApiProvider provider) {
+        super(provider);
+        this.provider = provider;
+    }
+
+    @Override
+    public void onInstrumentAdded(String alias, InstrumentInfo instrumentInfo) {
+        super.onInstrumentAdded(alias, instrumentInfo);
+        SwingUtilities.invokeLater(() -> {
+            createFrame();
+            combo.addItem(alias);
+        });
+    }
+
+    @Override
+    public void onInstrumentRemoved(String alias) {
+        super.onInstrumentRemoved(alias);
+        SwingUtilities.invokeLater(() -> {
+            combo.removeItem(alias);
+        });
+    }
+
+    private void createFrame() {
+        if (frame == null) {
+            frame = new JFrame() {
+                @Override
+                public void dispose() {
+                    super.dispose();
+                    frame = null;
+                }
+            };
+
+            frame.setSize(600, 400);
+            combo = new JComboBox<>(new String[]{NONE_ITEM});
+            combo.addItemListener(e -> {
+                if (e.getStateChange() == ItemEvent.SELECTED) {
+                    boolean itemSelected = !NONE_ITEM.equals(combo.getSelectedItem());
+                    
+                    Object alias = itemSelected ? combo.getSelectedItem() : "" ;
+                    frame.getRootPane().putClientProperty(Layer1ExternalAction.EXTERNAL_WINDOW_ALIAS_PROPERTY, alias);
+
+                    frame.setTitle(STRATEGY_NAME + (itemSelected ? "#" + alias : ""));
+                    aliasLabel.setText(itemSelected ? (String) alias : NONE_ITEM);
+                }
+            });
+            JLabel prefixLabel = new JLabel("Window belongs to alias: ");
+            aliasLabel = new JLabel(NONE_ITEM);
+            frame.setLayout(new FlowLayout());
+            frame.setTitle(STRATEGY_NAME);
+            frame.getRootPane().putClientProperty(Layer1ExternalAction.EXTERNAL_WINDOW_ALIAS_PROPERTY, "");
+            frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+            
+            frame.add(prefixLabel);
+            frame.add(combo);
+            frame.add(aliasLabel);
+            frame.setVisible(true);
+        }
+    }
+
+    @Override
+    public void finish() {
+        if (frame != null) {
+            frame.dispose();
+        }
+    }
+
+    /** This method will register action when the strategy is loaded */
+    @Layer1ActionMapper
+    public Layer1ActionContainer registerActions() {
+        Set<Layer1ExternalAction> actions = new HashSet<>();
+        actions.add(new TestExternalWindowAction());
+        return new Layer1ActionContainer(actions);
+    }
+    
+    @Layer1ActionMetadata(id = "Alert from external window")
+    public class TestExternalWindowAction implements Layer1ExternalAction {
+
+        @Override
+        public boolean performAction(String actionId, KeyEvent e) {
+            boolean isMyWindow = e.getComponent() == frame
+                    || SwingUtilities.getWindowAncestor(e.getComponent()) == frame;
+            
+            if (isMyWindow) {
+                String alias = (String) frame.getRootPane()
+                        .getClientProperty(Layer1ExternalAction.EXTERNAL_WINDOW_ALIAS_PROPERTY);
+                
+                String text = "".equals(alias) ? "Please choose the alias" : "Current alias: " + alias;
+                
+                provider.sendUserMessage(
+                        Layer1ApiSoundAlertMessage.builder()
+                                .setTextInfo("Alert from external window.\n\n" + text)
+                                .setSource(ActionStrategy2.class)
+                                .setMetadata(this.getClass().getName())
+                                .setShowPopup(true)
+                                .build()
+                );
+            }
+            return isMyWindow;
+        }
+
+        @Override
+        public void onShortcutChanged(String actionId, Set<String> shortcuts) {}
+    }
+}
---
+++ Strategies/src/main/java/velox/api/layer1/simpledemo/actions/ActionStrategy3.java
@@ -0,0 +1,132 @@
+package velox.api.layer1.simpledemo.actions;
+
+import velox.api.layer1.Layer1ApiFinishable;
+import velox.api.layer1.Layer1ApiInstrumentSpecificEnabledStateProvider;
+import velox.api.layer1.Layer1ApiProvider;
+import velox.api.layer1.actions.Layer1ActionContainer;
+import velox.api.layer1.actions.Layer1ExternalAction;
+import velox.api.layer1.actions.annotations.Layer1ActionMapper;
+import velox.api.layer1.actions.annotations.Layer1ActionMetadata;
+import velox.api.layer1.annotations.Layer1ApiVersion;
+import velox.api.layer1.annotations.Layer1ApiVersionValue;
+import velox.api.layer1.annotations.Layer1Attachable;
+import velox.api.layer1.annotations.Layer1Injectable;
+import velox.api.layer1.annotations.Layer1StrategyName;
+import velox.api.layer1.common.ListenableHelper;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.Color;
+import java.awt.Window;
+import java.awt.event.KeyEvent;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Strategy that declares custom actions and executes them in its external window.
+ *
+ * Unlike {@link ActionStrategy2} this strategy creates window per alias.
+ *
+ * To be able to perform actions in external window, such windows must have
+ * {@link Layer1ExternalAction#EXTERNAL_WINDOW_ALIAS_PROPERTY} client property.
+ *
+ * Additionally, this strategy requests Bookmap to execute built-in actions in the strategy window
+ * by declaring {@link Layer1ExternalAction#ALLOW_EXECUTING_BUILTIN_ACTIONS} client property which is set to true.
+ */
+@Layer1Attachable
+@Layer1Injectable
+@Layer1StrategyName("Action Strategy 3")
+@Layer1ApiVersion(Layer1ApiVersionValue.VERSION2)
+public class ActionStrategy3 implements Layer1ApiFinishable, Layer1ApiInstrumentSpecificEnabledStateProvider {
+    static final String STRATEGY_NAME = "Action Strategy 3";
+    private Layer1ApiProvider provider;
+    private Map<String, JFrame> aliasToFrame = new HashMap<>();
+
+    public ActionStrategy3(Layer1ApiProvider provider) {
+        this.provider = provider;
+        ListenableHelper.addListeners(provider, this);
+    }
+    
+    @Override
+    public void finish() {
+        aliasToFrame.values().forEach(Window::dispose);
+        aliasToFrame.clear();
+    }
+
+    /** This method will register action when the strategy is loaded */
+    @Layer1ActionMapper
+    public Layer1ActionContainer registerActions() {
+        Set<Layer1ExternalAction> actions = new HashSet<>();
+        actions.add(new ColorAction());
+        return new Layer1ActionContainer(actions);
+    }
+
+    @Override
+    public void onStrategyCheckboxEnabled(String alias, boolean isEnabled) {
+        if (isEnabled) {
+            JFrame frame = createFrame(alias);
+            aliasToFrame.put(alias, frame);
+            frame.setVisible(true);
+        } else {
+            JFrame frame = aliasToFrame.put(alias, null);
+            if (frame != null) {
+                frame.dispose();
+            }
+        }
+    }
+    
+    private JFrame createFrame(String alias) {
+        JFrame frame = new JFrame() {
+            @Override
+            public void dispose() {
+                super.dispose();
+                String alias = (String) this.getRootPane()
+                        .getClientProperty(Layer1ExternalAction.EXTERNAL_WINDOW_ALIAS_PROPERTY);
+                aliasToFrame.remove(alias);
+            }
+        };
+        frame.setSize(600, 400);
+        frame.setTitle(STRATEGY_NAME + "#" + alias);
+        frame.getRootPane().putClientProperty(Layer1ExternalAction.EXTERNAL_WINDOW_ALIAS_PROPERTY, alias);
+        frame.getRootPane().putClientProperty(Layer1ExternalAction.ALLOW_EXECUTING_BUILTIN_ACTIONS, true);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        JLabel label1 = new JLabel("Window belongs to alias: " + alias);
+        frame.add(label1);
+        return frame;
+    }
+
+    @Override
+    public boolean isStrategyEnabled(String alias) {
+        return aliasToFrame.containsKey(alias);
+    }
+
+
+    @Layer1ActionMetadata(id = ColorAction.RED_ACTION_ID, name = "Red Color")
+    @Layer1ActionMetadata(id = ColorAction.BLUE_ACTION_ID, name = "Blue Color")
+    public class ColorAction implements Layer1ExternalAction {
+        private static final String RED_ACTION_ID = "action_strategy3.red_color";
+        private static final String BLUE_ACTION_ID = "action_strategy3.blue_color";
+
+        @Override
+        public boolean performAction(String actionId, KeyEvent e) {
+            boolean isMyWindow = aliasToFrame.values().stream().anyMatch(frame -> e.getComponent() == frame);
+            
+            if (isMyWindow) {
+                JFrame frame = (JFrame) e.getComponent();
+                JLabel label = (JLabel) frame.getRootPane().getContentPane().getComponent(0);
+
+                Color color = RED_ACTION_ID.equals(actionId) ? Color.RED : Color.BLUE;
+                label.setForeground(color);
+            }
+            return isMyWindow;
+        }
+
+        @Override
+        public void onShortcutChanged(String actionId, Set<String> shortcuts) {
+            System.out.println("ActionStrategy3#onShortcutChanged: " + shortcuts);
+        }
+    }
+}
